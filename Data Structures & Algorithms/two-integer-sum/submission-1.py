ab582d4dfe873107d575f0cf755acd886a3b9a8e
VuDuc1610 <90380300+VuDuc1610@users.noreply.github.com>
2026-09-04 16:22:46 -0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        myMap = {}
+        for i in range(len(nums)):
+            temp = target-nums[i]
+            if temp in myMap:
+                return [myMap[temp], i]
+            myMap[nums[i]] = i 
+        return [0,3]
