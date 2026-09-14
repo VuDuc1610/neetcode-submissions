9d042f83465f96e21442c016b5b0e667c9acef16
VuDuc1610 <90380300+VuDuc1610@users.noreply.github.com>
2026-09-14 12:12:23 -0700

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        ptr = 0
+        if len(s2) < len(s1):
+            return False
+        map1,map2 = {},{}
+
+        for i in range(len(s1)):
+            map1[s1[i]] = 1 + map1.get(s1[i],0)
+
+        for i in range(len(s2)):
+            map2[s2[i]] = 1 + map2.get(s2[i],0)
+            if i >= len(s1)-1:
+                if map1 == map2:
+                    return True
+                map2[s2[ptr]] -= 1
+                if map2[s2[ptr]] == 0:
+                    map2.pop(s2[ptr])
+                ptr += 1
+        return False
